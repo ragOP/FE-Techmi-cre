7033e38880c2480d00c1ca032d8bde8e7d535575
Kashif <kashifd.tanikatech@gmail.com>
2025-02-21 22:19:16 +0530

    login page and app routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/home"
-import Service from "./pages/services"
-import Navbar from "./components/navbar"
-import Footer from "./components/footer"
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/home";
+import Service from "./pages/services";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Login from "./pages/login";
 
 const App = () => {
+  const location = useLocation(); // Get the current route
+
+  // Define routes that should NOT include Navbar & Footer
+  const hideLayoutRoutes = ["/login"];
+
   return (
     <div className="bg-[#82c8e51a]">
-      <Navbar />
+      {/* Show Navbar only if route is NOT in hideLayoutRoutes */}
+      {!hideLayoutRoutes.includes(location.pathname) && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/service" element={<Service />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-      <Footer />
-    </div>
-  )
-}
 
-export default App
+      {/* Show Footer only if route is NOT in hideLayoutRoutes */}
+      {!hideLayoutRoutes.includes(location.pathname) && <Footer />}
+    </div>
+  );
+};
+
+export default App;

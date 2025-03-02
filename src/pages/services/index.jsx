a2ba117843fed20d361cc09f7453ac610c28dcbf
farish-jamal <farishjamal98@gmail.com>
2025-03-03 01:12:19 +0530

    Fix scroll in Service Page

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import CategoryGrid from "../../components/home/Categ_Options";
 import HouseCleaning from "../../components/service/HouseCleaning";
 import Pharmaceutical from "../../components/service/Pharmaceutical";
 import LaundryService from "../../components/service/LaundryService";
 import Tagtitle from "../../components/service/service_components/Pharmaceutical_Services/tagtitle";
+import ServiceDescription from "../../components/service_description";
+import { description } from "../../constant";
 
 const Service = () => {
   const location = useLocation();
   const selectedCardTitle = location.state?.name || null;
-  
+
+  const houseCleaningRef = useRef(null);
+  const pharmaceuticalRef = useRef(null);
+  const laundryServiceRef = useRef(null);
+  const serviceRef = useRef(null);
+
+  const scrollToRef = (ref) => {
+    setTimeout(() => {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }, 500);
+  };
+
+  useEffect(() => {
+    switch (selectedCardTitle) {
+      case "House Cleaning":
+        scrollToRef(houseCleaningRef);
+        break;
+      case "Pharmaceutical":
+        scrollToRef(pharmaceuticalRef);
+        break;
+      case "Laundry Service":
+        scrollToRef(laundryServiceRef);
+        break;
+      default:
+        scrollToRef(serviceRef);
+        break;
+    }
+  }, [selectedCardTitle]);
+
   return (
     <div>
       <Tagtitle />
@@ -17,9 +47,37 @@ const Service = () => {
 
       {selectedCardTitle && (
         <div className="mt-6">
-          {selectedCardTitle === "House Cleaning" && <HouseCleaning />}
-          {selectedCardTitle === "Pharmaceutical" && <Pharmaceutical />}
-          {selectedCardTitle === "Laundry Service" && <LaundryService />}
+          <div ref={houseCleaningRef}>
+            {selectedCardTitle === "House Cleaning" && <HouseCleaning />}
+          </div>
+          <div ref={pharmaceuticalRef}>
+            {selectedCardTitle === "Pharmaceutical" && <Pharmaceutical />}
+          </div>
+          <div ref={laundryServiceRef}>
+            {selectedCardTitle === "Laundry Service" && <LaundryService />}
+          </div>
+        </div>
+      )}
+      {!selectedCardTitle && (
+        <div ref={serviceRef}>
+          <ServiceDescription
+            reverse={false}
+            title="House Cleaning"
+            image="https://res.cloudinary.com/dacwig3xk/image/upload/v1740342553/uploads/images/wrmpnukabjaqfpbzuehl.png"
+            description={description[0]}
+          />
+          <ServiceDescription
+            reverse={true}
+            title="Pharmaceutical"
+            image="https://res.cloudinary.com/dacwig3xk/image/upload/v1740342674/uploads/images/ou5nt9qf4yctozmcwgwc.png"
+            description={description[1]}
+          />
+          <ServiceDescription
+            reverse={false}
+            title="Laundry Services"
+            image="https://res.cloudinary.com/dacwig3xk/image/upload/v1740342717/uploads/images/jvg1lophws6vvjsudjcy.png"
+            description={description[2]}
+          />
         </div>
       )}
     </div>
